994e3a0b5fe4afffa4a74911033bbd8ca8bc8abd
peterylh <peterylh@163.com>
2026-05-30 13:28:31 +0800

    chore: remove scratch file

--- scratch.py
+++
@@ -1,5 +0,0 @@
-from config import get_naming_config
-import pprint
-nc = get_naming_config("shop", "naming_config_enterprise.yaml")
-td = nc.types.get("BIZ_PROCESS")
-print(repr(td.regex), "_" in (td.regex or ""))
